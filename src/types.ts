add4d4f408177615c5a87783a3cbb0e149f7a156
eggnom <h75408706@gmail.com>
2023-01-25 11:12:02 +0300

    add users service

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 export const TYPES = {
 	Application: Symbol.for('Application'),
 	ILogger: Symbol.for('ILogger'),
+	UserService: Symbol.for('UserService'),
 	UserController: Symbol.for('UserController'),
 	ExeptionFilter: Symbol.for('ExeptionFilter'),
 };
